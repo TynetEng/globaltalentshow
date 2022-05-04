e8b02c8a9e297cf9fb740329b12386a2be19296b
TynetEng <agbesuaoluwatoyin96@yahoo.com>
2022-05-04 16:54:12 +0100

    users details edited

--- config/auth.php
+++ config/auth.php
@@ -15,9 +15,7 @@ return [
 
     'defaults' => [
         'guard' => 'web',
-        'passwords' => 'admins',
-        'passwords' => 'voters',
-        'passwords' => 'contestants',
+        'passwords' => 'users',
     ],
 
     /*
@@ -38,10 +36,10 @@ return [
     */
 
     'guards' => [
-        // 'user' => [
-        //     'driver' => 'session',
-        //     'provider' => 'users',
-        // ],
+        'user' => [
+            'driver' => 'session',
+            'provider' => 'users',
+        ],
         'admin' => [
             'driver' => 'session',
             'provider' => 'admins',
@@ -74,10 +72,10 @@ return [
     */
 
     'providers' => [
-        // 'users' => [
-        //     'driver' => 'eloquent',
-        //     'model' => App\Models\User::class,
-        // ],
+        'users' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\User::class,
+        ],
 
         'admins' => [
             'driver' => 'eloquent',
@@ -114,12 +112,12 @@ return [
     */
 
     'passwords' => [
-        // 'users' => [
-        //     'provider' => 'users',
-        //     'table' => 'password_resets',
-        //     'expire' => 60,
-        //     'throttle' => 60,
-        // ],
+        'users' => [
+            'provider' => 'users',
+            'table' => 'password_resets',
+            'expire' => 60,
+            'throttle' => 60,
+        ],
         'admins' => [
             'provider' => 'admins',
             'table' => 'password_resets',
