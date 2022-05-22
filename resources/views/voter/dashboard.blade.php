8da6c06f0e6ef7a96780443fa04ed2e79beff15d
TynetEng <agbesuaoluwatoyin96@yahoo.com>
2022-05-22 05:51:09 +0100

    paystack database updated

--- resources/views/voter/dashboard.blade.php
+++ resources/views/voter/dashboard.blade.php
@@ -66,7 +66,22 @@
                             </div>
 
                             
-                            
+                            <form action="{{url('paypal')}}" method="post">
+                                @if(\Session::has('error'))
+                                    <div class="alert alert-danger">{{ \Session::get('error') }}</div>
+                                    {{ \Session::forget('error') }}
+                                @endif
+                                @if(\Session::has('success'))
+                                    <div class="alert alert-success">{{ \Session::get('success') }}</div>
+                                    {{ \Session::forget('success') }}
+                                @endif
+
+                                <div class="take">
+                                    <a class="btn btn-primary" href="{{ route('processTransaction') }}">Pay $5 for vote with Paypal</a>
+                                </div>
+                                
+                                @csrf
+                            </form>
 
                             
                             
