b6521516ed092a0fc5902959988b747eb9547411
TynetEng <agbesuaoluwatoyin96@yahoo.com>
2022-06-08 15:07:54 +0100

    signup edited

--- config/services.php
+++ config/services.php
@@ -31,7 +31,7 @@ return [
     ],
     'google' => [
         'client_id' => '874572811698-bpfsmp0m2j46qmpkh0bcd5l0ccbgdh3v.apps.googleusercontent.com',
-        'client_secret' => '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',
+        'client_secret' => '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',
         'redirect' => 'https://globaltalentshow.herokuapp.com/auth/google/callback',
     ],
 
