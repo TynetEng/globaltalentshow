002b86ffe665946e15c1d8c84fc8ff0e5fce50b4
TynetEng <agbesuaoluwatoyin96@yahoo.com>
2022-06-10 05:29:31 +0100

    signup edited

--- resources/views/contestant/login.blade.php
+++ resources/views/contestant/login.blade.php
@@ -153,9 +153,6 @@
         .pat:hover{
             color:rgb(77, 14, 26);
         }
-        input[type="checkbox" i]{
-            background-color: rgb(154, 3, 30) !important;
-        }
 
         footer{
             text-align: center;
